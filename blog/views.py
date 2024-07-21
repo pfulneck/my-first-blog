4ef21c2dae97684ba5b940f5b79d94516c9564fc
pfulneck <pfulneck@gmail.com>
2024-07-21 11:30:52 -0400

    Changed views, base, and post_detail.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 
@@ -36,6 +37,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
